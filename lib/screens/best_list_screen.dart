791afe6e620ddf66731d9ea76fffb56169ff3eeb
seunghoon <rhfqoddl555@gmail.com>
2024-02-15 01:36:06 +0900

    괄호 처리

--- lib/screens/best_list_screen.dart
+++ lib/screens/best_list_screen.dart
@@ -22,30 +22,29 @@ class BestListScreen extends StatelessWidget {
             future: ApiService().searchProd(kwds, isBestUrl),
             builder: (context, snapshot) {
               if (snapshot.connectionState == ConnectionState.waiting) {
-                return  const Center(child: TextContentBox( mainText: "상품이 준비중입니다. 잠시만 기다려주세요"));
+                return const Center(
+                    child: TextContentBox(mainText: "상품이 준비중입니다. 잠시만 기다려주세요"));
               } else if (snapshot.hasError) {
                 return Text("오류가 발생했습니다\n ${snapshot.error}");
               } else if (snapshot.hasData) {
                 return ListView(
-                  children: [Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        '$title 카테고리의 베스트 상품 입니다. \n총 ${snapshot.data!.prods.length}개 상품이 준비되어있습니다.',
-                        style: const TextStyle(
-                          fontSize: 24,
-                          color: GRAY_COLOR,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-
-                    ),
+                  children: [
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '$title 카테고리의 베스트 상품 입니다. \n총 ${snapshot.data!.prods.length}개 상품이 준비되어있습니다.',
+                            style: const TextStyle(
+                              fontSize: 24,
+                              color: GRAY_COLOR,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ]),
                     Expanded(
                       child: Semantics(
                         label: "상품리스트",
-                                                      container: true,
-
-
+                        container: true,
                         child: ListView.builder(
                           itemCount: snapshot.data!.prods.length,
                           itemBuilder: (context, index) {
@@ -56,7 +55,6 @@ class BestListScreen extends StatelessWidget {
                                   prod: prod,
                                 ));
                               },
-
                               child: Semantics(
                                 button: true,
                                 label: "상품",
@@ -67,7 +65,8 @@ class BestListScreen extends StatelessWidget {
                                   margin: const EdgeInsets.symmetric(
                                       horizontal: 16.0, vertical: 24.0),
                                   child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
                                     children: [
                                       Text(
                                         '${prod.name}(${prod.ratingNum})',
@@ -81,7 +80,8 @@ class BestListScreen extends StatelessWidget {
                                           style: TextStyle(
                                               fontSize: 12.0,
                                               color: Colors.grey[500],
-                                              decoration: TextDecoration.lineThrough),
+                                              decoration:
+                                                  TextDecoration.lineThrough),
                                         ),
                                       Text(
                                         prod.price,
@@ -92,15 +92,13 @@ class BestListScreen extends StatelessWidget {
                                     ],
                                   ), // 여기서 prod 객체의 속성을 사용하여 위젯을 구성합니다.
                                 ),
-
                               ),
                             );
                           },
                         ),
                       ),
-                    ],
-                  ),
-              ],
+                    ),
+                  ],
                 );
               } else {
                 return const Text("데이터가 없습니다.");
